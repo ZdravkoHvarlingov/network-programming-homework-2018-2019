64ce62b3671f122c7e251d080a87e16add4fb0c4
Zdravko Hvarlingov <zdravkohv@gmail.com>
2018-12-23 21:44:01 +0200

    Multicast chat server with text messages

--- com/fmi/mpr/hw/chat/src/Client.java
+++ com/fmi/mpr/hw/chat/src/Client.java
@@ -1,30 +1,117 @@
 import java.io.IOException;
 import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
-public class Client {
+public class Client implements Runnable {
+
+	final static String MULTICAST_INET_ADDR = "224.0.0.3";
+	final static int MULTICAST_PORT = 6565;
 	
-	final static String INET_ADDR = "224.0.0.3";
-	final static int PORT = 6565;
+	final static String SERVER_INET_ADDR = "localhost";
+	final static int SERVER_PORT = 6566;
+	
 	final static int MAX_LENGTH = 1024;
+	static Boolean KEEP_READING = true;
 	
-	public static void main(String[] args) throws IOException {
-		InetAddress address = InetAddress.getByName(INET_ADDR);
+	static String name = null;
+	
+	public static void main(String[] args) throws IOException, InterruptedException {
+		DatagramSocket sendSocket = new DatagramSocket();
+		InetAddress address = InetAddress.getByName(SERVER_INET_ADDR);
+		
+		Scanner scanner = new Scanner(System.in);
+		HandleNameRegistration(scanner, sendSocket, address);
+		
+		Thread readMessages = new Thread(new Client());
+		readMessages.start();
+		
+		String message;
+		message = scanner.nextLine();
+		
+		while(!message.equals("exit"))
+		{
+			message = name + ": " + message;
+			DatagramPacket packetToSend = new DatagramPacket(message.getBytes(), message.getBytes().length, address, SERVER_PORT);
+			sendSocket.send(packetToSend);
+			
+			message = scanner.nextLine();
+		}
+		
+		KEEP_READING = false;
+		HandleLogout(sendSocket, address);
+		readMessages.join();
+		scanner.close();
+		sendSocket.close();
+	}
+	
+	public static void HandleNameRegistration(Scanner scanner, DatagramSocket sendSocket, InetAddress address) throws IOException {
+		
+		System.out.println("Enter chat name(only letters and digits): ");
+		name = scanner.nextLine();
+		name = "#" + name; 
+		
+		DatagramPacket regPacket = new DatagramPacket(name.getBytes(), name.getBytes().length, address, SERVER_PORT);
+		sendSocket.send(regPacket);
 		
 		byte[] buff = new byte[MAX_LENGTH];
-		try(MulticastSocket clientSocket = new MulticastSocket(PORT)) {
+		DatagramPacket regAnswer = new DatagramPacket(buff, buff.length);
+		sendSocket.receive(regAnswer);
+		String regAnswerToString = new String(regAnswer.getData(), regAnswer.getOffset(), regAnswer.getLength());
+
+		while(!regAnswerToString.equals("Success")) {
+			System.out.println("Chat name is already used, enter another one: ");
+			name = scanner.nextLine();
+			name = "#" + name; 
+			
+			regPacket = new DatagramPacket(name.getBytes(), name.getBytes().length, address, SERVER_PORT);
+			sendSocket.send(regPacket);
+			regAnswer = new DatagramPacket(buff, buff.length);
+			sendSocket.receive(regAnswer);
+			regAnswerToString = new String(regAnswer.getData(), regAnswer.getOffset(), regAnswer.getLength());
+		}
+		
+		System.out.println("Chat entered successfuly!");
+		name = name.substring(1);
+	}
+	
+	private static void HandleLogout(DatagramSocket sendSocket, InetAddress address) throws IOException {
+		
+		String msg = "?" + name;
+		DatagramPacket logoutPacket = new DatagramPacket(msg.getBytes(), msg.getBytes().length, address, SERVER_PORT);
+		sendSocket.send(logoutPacket);
+	}
+	
+	@Override
+	public void run() {
+		InetAddress address = null;
+		try {
+			address = InetAddress.getByName(MULTICAST_INET_ADDR);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		byte[] buff = new byte[MAX_LENGTH];
+		try(MulticastSocket clientSocket = new MulticastSocket(MULTICAST_PORT)) {
 			clientSocket.joinGroup(address);
 			
-			System.out.println("Client is waiting for messages:");
-			
-			while(true) {
+			while(KEEP_READING) {
 				DatagramPacket receivedPacket = new DatagramPacket(buff, buff.length);
 				clientSocket.receive(receivedPacket);
 				
-				String receivedMsg = new String(buff, 0, buff.length);
-				System.out.println("Client received: " + receivedMsg);
+				String receivedMsg = new String(receivedPacket.getData(), receivedPacket.getOffset(), receivedPacket.getLength());
+				if(!receivedMsg.split(":")[0].equals(name)) {
+
+					System.out.println(receivedMsg);	
+				}
 			}
-		}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
 	}
 }
--- com/fmi/mpr/hw/chat/src/Server.java
+++ com/fmi/mpr/hw/chat/src/Server.java
@@ -2,27 +2,68 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Server {
 	
-	final static String INET_ADDR = "224.0.0.3";
-	final static int PORT = 6565;
+	final static String MULTICAST_INET_ADDR = "224.0.0.3";
+	final static int MULTICAST_PORT = 6565;
+	final static int SERVER_PORT = 6566;
+	final static int MAX_LENGTH = 1024;
+	
+	static Set<String> connectedNames;
 	
 	public static void main(String[] args) throws IOException, InterruptedException {
-		InetAddress address = InetAddress.getByName(INET_ADDR);
+		connectedNames = new TreeSet<String>();
 		
-		try(DatagramSocket serverSocket = new DatagramSocket()) {
-			System.out.println("Server started!");
+		try(DatagramSocket receiveSocket = new DatagramSocket(SERVER_PORT)) {		
+			byte[] buff = new byte[MAX_LENGTH];	
 			
-			for (int i = 0; i < 5; i++) {
-				String newMsg = "Hahaha, new message N:" + i;
+			while(true) {
+				DatagramPacket receivedPacket  = new DatagramPacket(buff, buff.length);
+				receiveSocket.receive(receivedPacket);
 				
-				DatagramPacket sendPacket = new DatagramPacket(newMsg.getBytes(), newMsg.getBytes().length, address, PORT);
-				serverSocket.send(sendPacket);
+				String msg = new String(receivedPacket.getData(), receivedPacket.getOffset(), receivedPacket.getLength());
+				if(msg.startsWith("#")) {
+					ProcessNewName(msg.substring(1), receiveSocket, receivedPacket.getAddress(), receivedPacket.getPort());
+					msg = msg.substring(1) + " entered the chat...";
+				}
+				else if(msg.startsWith("?")) {
+					LogoutUser(msg.substring(1));
+					msg = msg.substring(1) + " left the chat...";
+				}
+				else if(connectedNames.contains(msg.split(":")[0])) {
+					System.out.println(msg);
+				}
 				
-				System.out.println("Message sent: " + newMsg);
-				Thread.sleep(500);
+				InetAddress address = InetAddress.getByName(MULTICAST_INET_ADDR);
+				DatagramPacket multicastPacket = new DatagramPacket(msg.getBytes(), msg.getBytes().length, address, MULTICAST_PORT);
+				receiveSocket.send(multicastPacket);
 			}
+		}
+		
+	}
+	
+	private static void ProcessNewName(String name, DatagramSocket socket, InetAddress address, int port) throws IOException {
+		String regAnswer;
+		if(connectedNames.contains(name)) {
+			regAnswer = "Name is already used";
+		}
+		else {
+			regAnswer = "Success";
+			connectedNames.add(name);
+			System.out.println(name + " entered the chat!");
+		}
+		
+		DatagramPacket answer = new DatagramPacket(regAnswer.getBytes(), regAnswer.getBytes().length, address, port);
+		socket.send(answer);
+	}
+	
+	private static void LogoutUser(String name) {
+		if(connectedNames.contains(name)) {
+			connectedNames.remove(name);
+			System.out.println(name + " left the chat...");
 		}
 	}
 
